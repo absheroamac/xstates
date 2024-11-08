0df3e42cb76a6f02062e8c8df2c29f975a2d1fc9
Absher K <absheroamac@gmail.com>
2024-11-09 01:47:22 +0530

    updated code

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   const [countries, setCountries] = useState([]);
   const [states, setStates] = useState([]);
   const [cities, setCities] = useState([]);
-  const [selectedCoutry, setSelectedCountry] = useState("");
+  const [selectedCountry, setSelectedCountry] = useState("");
   const [selectedState, setSelectedState] = useState("");
   const [selectedCity, setSelectedCity] = useState("");
 
@@ -16,9 +16,9 @@ function App() {
         );
         const data = await res.json();
         setCountries(data);
-        console.log(data);
       } catch (err) {
         console.error(err);
+        setCountries([]);
       }
     };
 
@@ -29,28 +29,26 @@ function App() {
     const getCountries = async () => {
       try {
         const res = await fetch(
-          `https://crio-location-selector.onrender.com/country=${selectedCoutry}/states`
+          `https://crio-location-selector.onrender.com/country=${selectedCountry}/states`
         );
         const data = await res.json();
         setStates(data);
-        console.log(data);
       } catch (err) {
         console.error(err);
       }
     };
 
     getCountries();
-  }, [selectedCoutry]);
+  }, [selectedCountry]);
 
   useEffect(() => {
     const getCountries = async () => {
       try {
         const res = await fetch(
-          `https://crio-location-selector.onrender.com/country=${selectedCoutry}/state=${selectedState}/cities`
+          `https://crio-location-selector.onrender.com/country=${selectedCountry}/state=${selectedState}/cities`
         );
         const data = await res.json();
         setCities(data);
-        console.log(data);
       } catch (err) {
         console.error(err);
       }
@@ -62,43 +60,42 @@ function App() {
   return (
     <div className="App" style={{ fontFamily: "sans-serif" }}>
       <h1>Select Location</h1>
-      <div>
-        <select
-          disabled={countries.length === 0}
-          onChange={(event) => setSelectedCountry(event.target.value)}
-        >
-          <option value="" disabled selected>
-            Select Country
-          </option>
-          {countries.length > 0 &&
-            countries.map((item) => <option value={item}>{item}</option>)}
-        </select>
+      <select
+        disabled={countries.length === 0}
+        onChange={(event) => setSelectedCountry(event.target.value)}
+      >
+        <option value="" disabled selected>
+          Select Country
+        </option>
+        {countries.length > 0 &&
+          countries.map((item) => <option value={item}>{item}</option>)}
+      </select>
 
-        <select
-          disabled={states.length === 0}
-          onChange={(event) => setSelectedState(event.target.value)}
-        >
-          <option value="" disabled selected>
-            Select State
-          </option>
-          {states.length > 0 &&
-            states.map((item) => <option value={item}>{item}</option>)}
-        </select>
+      <select
+        disabled={states.length === 0}
+        onChange={(event) => setSelectedState(event.target.value)}
+      >
+        <option value="" disabled selected>
+          Select State
+        </option>
+        {states.length > 0 &&
+          states.map((item) => <option value={item}>{item}</option>)}
+      </select>
 
-        <select
-          disabled={cities.length === 0}
-          onChange={(event) => setSelectedCity(event.target.value)}
-        >
-          <option value="" disabled selected>
-            Select City
-          </option>
-          {cities.length > 0 &&
-            cities.map((item) => <option value={item}>{item}</option>)}
-        </select>
-      </div>
+      <select
+        disabled={cities.length === 0}
+        onChange={(event) => setSelectedCity(event.target.value)}
+      >
+        <option value="" disabled selected>
+          Select City
+        </option>
+        {cities.length > 0 &&
+          cities.map((item) => <option value={item}>{item}</option>)}
+      </select>
+
       {selectedCity !== "" && (
         <p>
-          You Selected {selectedCity}, {selectedState}, {selectedCoutry}
+          You Selected {selectedCity}, {selectedState}, {selectedCountry}
         </p>
       )}
     </div>
